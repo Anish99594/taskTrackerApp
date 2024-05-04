a12dc2ba54983f703e03a83a731bccec3a61d0ca
Anish Gajbhare <64015968+Anish99594@users.noreply.github.com>
2024-05-04 21:12:53 +0530

    Update index.js

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -16,12 +16,12 @@ export const mutations = {
     },
     REMOVE_TASK(state, task){
         state.tasks.splice(state.tasks.indexOf(task), 1);
-        axios.delete(`http://localhost:3001/${task.content._id}`);
+        axios.delete(`https://task-tracker-app-backend.vercel.app/${task.content._id}`);
     },
     TOGGLE_TASK(state, task){
         task.content.done = !task.content.done;
         
-        axios.put(`http://localhost:3001/${task.content._id}`, {done: true})
+        axios.put(`https://task-tracker-app-backend.vercel.app/${task.content._id}`, {done: true})
         .then((response)=>{
             console.log(response);
         }).catch((error)=>{
